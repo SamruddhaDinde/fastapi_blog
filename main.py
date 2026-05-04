17914fecb6d52841ad38e0c5fd09ad0527d11fb6
SamruddhaDinde <samruddha.dinde@gmail.com>
2026-05-04 17:10:59 +1000

    implemented patch update for user

--- main.py
+++ main.py
@@ -127,7 +127,45 @@ def get_user_posts(user_id:int, db: Annotated[Session, Depends(get_db)]):
   posts = result.scalars().all()
   return posts
 
+@app.patch("/api/users/{user_id}", response_model= UserResponse)
+def update_user(user_id: int, user_update: UserUpdate, db: Annotated[Session, Depends(get_db)]):
+  result = db.execute(select(models.User).where(models.User.id == user_id))
+  user = result.scalars().first()
+  if not user:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail="User not found"
+    )
+  if user_update.username is not None and user_update.username != user.username:
+    result = db.execute(select(models.User).where(models.User.username == user_update.username))
+    exiting_user = result.scalars().first()
+    if exiting_user:
+      raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="User already exists"
+      )
+  if user_update.email is not None and user_update.email != user.email:
+    result = db.execute(select(models.User).where(models.User.email == user_update.email))
+    exiting_email = result.scalars().first()
+    if exiting_email:
+      raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="email already exists"
+      )
+    
+  if user_update.email is not None:
+      user.email = user_update.email
+  if user_update.username is not None:
+      user.username = user_update.username
+  if user_update.image_file is not None:
+      user.image_file = user_update.image_file
 
+  db.commit()
+  db.refresh(user)
+  return user
+  
+
+@app.delete("/api/users")
 
 @app.get("/api/posts", response_model=list[PostResponse])
 def get_posts(db: Annotated[Session, Depends(get_db)]):
